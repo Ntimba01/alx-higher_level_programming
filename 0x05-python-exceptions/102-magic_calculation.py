c1fecdd73963d127c3c901d9abd7ede0407030fe
ntimba01 <ntimba01@gmail.com>
2023-10-23 15:58:01 -0700

    updated

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,5 @@
+>>> dis.dis(myfunc)
+  2           0 LOAD_GLOBAL              0 (len)
+              3 LOAD_FAST                0 (alist)
+              6 CALL_FUNCTION            1
+              9 RETURN_VALUE
